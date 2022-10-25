8901fb23798cfe55a741c1fbdfe0138d493f0d52
ffm15 <116546303+ffm1515@users.noreply.github.com>
2022-10-25 10:48:45 +0000

    Vor der Pause

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from browser import document, window, alert
 import random 
+ 
 
 def sketch(p): 
   #this p is needed. it will be the processing sketch itself.
   # to do things like background(0) instead do p.background(0)
   def setup():
-    p.createCanvas(700, 410)
-    # Die Hintergrundfarbe kann Verändert werden, in dem wir den Übergeben wert verändern.
-    # Diesen übergebenen Wert nennt man einen Parameter.
-    # Die runden Klammern >()< die diesen Wert umschließen, bezeichnet man als Parameterliste.
+    p.createCanvas(700,400)
     p.background(255)
     p.rectMode(p.CENTER)
+    p.circle(230,120,50)
+    p.rect(230, 120, 120, 40)
+    p.circle(230,120,30)
+    p.rect(230,120,100,20)
+    p.circle(230,120,20)
   p.setup = setup
-
+    
 myp5 = window.p5.new(sketch)
+                     
